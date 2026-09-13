afaf62329c25a3285b8f2136691ec5079d6f0d29
bryanmsombra27 <bryanjm96@gmail.com>
2026-09-13 13:28:01 -0600

    adding pull to refresh

--- src/app/pull-to-refresh/index.tsx
+++ src/app/pull-to-refresh/index.tsx
@@ -1,11 +1,35 @@
+import { useTheme } from "@/hooks/use-theme";
 import ThemeText from "@/presentation/shared/ThemeText";
-import { View } from "react-native";
+import ThemeView from "@/presentation/shared/ThemeView";
+import { useState } from "react";
+import { RefreshControl, ScrollView } from "react-native";
 
 const PullToRefreshScreen = () => {
+  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
+  const { primary } = useTheme();
+
+  const Onrefresh = async () => {
+    setIsRefreshing(true);
+
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 3000);
+  };
+
   return (
-    <View>
-      <ThemeText className="text-white">PullToRefreshScreen</ThemeText>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={Onrefresh}
+          colors={[primary, "red", "orange", "green"]}
+        />
+      }
+    >
+      <ThemeView margin>
+        <ThemeText className="text-white">PullToRefreshScreen</ThemeText>
+      </ThemeView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
